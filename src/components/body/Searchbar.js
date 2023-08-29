0fd1d546ebd2ef0268f97ea131c3f636b80feedc
Hritik <pypi.pioneer@gmail.com>
2023-08-30 01:43:15 +0530

    icons improved

--- src/components/body/Searchbar.js
+++ src/components/body/Searchbar.js
@@ -3,17 +3,18 @@ import React from 'react';
 export default function Searchbar() {
     return (
         <>
-            <nav className="navbar navbar-light d-flex justify-content-end">
+            <nav className="navbar d-flex justify-content-end mt-3 mb-3">
 
                     <form className="form-inline">
 
                         {/* only visible when clicked on search icon */}
                         {/* <div className='nav-link'>
-                            <input id="placeholder-color" className="placeholder-red-300 form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" style={{ borderRadius: '20px' }} />
+                            <input id="placeholder-color" className="placeholder-red-300 form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" style={{ borderRadius: '10px' }} />
                         </div> */}
 
                         {/* search icon */}
-                        <i className="fa-solid fa-magnifying-glass mr-3" style={{fontSize: "30px"}}></i>
+                        <i className="fa-solid fa-magnifying-glass mr-4" style={{fontSize: "1.5em"}}></i>
+                        <i class="fa-solid fa-right-from-bracket mr-3" style={{fontSize: "1.5em"}}></i>
                     </form>
             </nav>
         </>
--- src/components/header/Navbar.js
+++ src/components/header/Navbar.js
@@ -4,7 +4,7 @@ import Toggle from './Toggle';
 export default function Navbar() {
     return (
         <>
-            <nav className="navbar navbar-light bg-light">
+            <nav className="navbar navbar-light">
                 <span className="navbar-brand mb-2 ml-2 mt-3" style={{fontSize: "2em", fontFamily: "Georgia"}}>Navbar</span>
                 <Toggle />
             </nav>
