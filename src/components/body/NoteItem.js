2b95fac5a898b9439846d374dfa463216548a3e1
Hritik <pypi.pioneer@gmail.com>
2023-08-30 18:13:59 +0530

    icnos added correctly

--- src/components/body/NoteItem.js
+++ src/components/body/NoteItem.js
@@ -9,7 +9,7 @@ export default function NoteItem(props) {
                 <Grid className="card-body">
 
                     <Grid item lg={6} className="d-inline-block mr-5" style={{ fontFamily: "Georgia", fontSize: '1em', fontStyle: 'italic', fontWeight: 'bold', marginTop: '-12px' }}>{props.title}</Grid>
-                    <i className="fa-solid fa-pen-to-square mx-4" style={{fontSize: "1.3em", paddingLeft: '5px'}}></i>
+                    <i className="fa-solid fa-pen-to-square" style={{fontSize: "1.2em", paddingLeft: '5px', float: 'right'}}></i>
                     
                     
                     
@@ -19,7 +19,7 @@ export default function NoteItem(props) {
 
                         <p className="card-text d-inline-block" style={{ fontSize: '0.8em', color: '#A9A9A9', marginBottom: '-3px' }}>Aug 30, 3:29 AM</p>
                         <div className="text-center d-inline-block mx-5" style={{ paddingLeft: '10px', paddingRight: '10px', borderStyle: 'solid', borderRadius: '10px', color: 'white', backgroundColor: '#4B0082', marginBottom: '-3px' }}>{props.tag}</div>
-                        <i className="fa-solid fa-trash mx-4" style={{ fontSize: "1.2em" }}></i>
+                        <i className="fa-solid fa-trash" style={{ fontSize: "1.2em", float: 'right' }}></i>
                     </Grid>
 
 
