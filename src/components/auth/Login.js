06c99d7c7833cf64e7aa2668f88809d4eb99a123
Hritik <pypi.pioneer@gmail.com>
2023-09-03 01:26:33 +0530

    ui created

---
+++ src/components/auth/Login.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import { Grid, Box, Modal } from '@mui/material';
+import './style.css';
+
+// styling for modal structure
+const style = {
+    bgcolor: 'background.paper',
+    borderRadius: '10px',
+    boxShadow: 24,
+};
+
+export default function Login() {
+    return (
+        <>
+            <Modal className='d-flex justify-content-center'
+                open={true}
+            >
+                <Box sx={Object.assign(style, {})} className='box-register box-login'>
+
+                    <Grid container className='grid-container-position'>
+
+                        <div className='register-label' style={{fontSize: "1.6em", fontFamily: "Georgia", fontWeight: 'bold'}}>Login Please</div>
+
+                        {/* email */}
+                        <Grid item lg={12} md={12} sm={12} xs={12} className='d-flex justify-content-center'>
+                            <input id="email-field" style={{...{fontFamily: "Georgia"} }} placeholder="Email"/>
+                        </Grid>
+
+                        {/* password */}
+                        <Grid item lg={12} md={12} sm={12} xs={12} className='d-flex justify-content-center'>
+                            <input id="password-field" type="password" style={{ ...{fontFamily: "Georgia"} }} placeholder="Password"></input>
+                        </Grid>
+
+                        <Grid item lg={12} md={12} sm={12} xs={12} className='d-flex justify-content-center'>
+                            <button id="button-field" type="button">Login</button>
+                        </Grid>
+
+                        <div className='account'>Forgot Password</div>
+                    </Grid>
+                </Box>
+            </Modal>
+        </>
+    )
+}
