24f8ff0ff45e5ee81f7e22a8c848ac8ccf2d1660
Hritik <pypi.pioneer@gmail.com>
2023-09-03 05:49:09 +0530

    context resolved and import removed too

--- src/App.js
+++ src/App.js
@@ -2,9 +2,6 @@ import './App.css';
 import Navbar from './components/header/Navbar';
 import Notebox from './components/body/Notebox';
 import { FetchNoteProvider } from './contexts/FetchNoteContext';
-import { DeleteNoteProvider } from './contexts/DeleteNoteContext';
-import { UpdateNoteProvider } from './contexts/UpdateNoteContext';
-import { AddNoteProvider } from './contexts/AddNoteContext';
 import Login from './components/auth/Login';
 import Signup from './components/auth/Signup';
 
