dcb8a38c05032e514b89cbfc5666615609f5aff4
Hritik <pypi.pioneer@gmail.com>
2023-09-05 16:54:26 +0530

    validating login fields

--- src/components/auth/Login.js
+++ src/components/auth/Login.js
@@ -15,6 +15,19 @@ const style = {
 // to make request to api, we need host
 const host = 'http://192.168.0.102:3100';
 
+// writing function to validate the login
+const validate = (values) => {
+    const errors = {};
+
+    if(!values.email) errors.email = 'Required';
+    else if(values.email.length <= 0) errors.email = 'Enter a valid Email';
+
+    if(!values.password) errors.password = 'Required';
+    else if(values.password.length < 6) errors.password = 'Enter a valid Password';
+
+    return errors;
+};
+
 export default function Login(props) {
 
     // state variables
@@ -53,6 +66,7 @@ export default function Login(props) {
             email: '',
             password: '',
         },
+        validate,
         onSubmit: values => {  // to login user after submission
             loginUser(values.email, values.password);
         },
@@ -72,12 +86,12 @@ export default function Login(props) {
 
                         {/* email */}
                         <Grid item lg={12} md={12} sm={12} xs={12} className='d-flex justify-content-center'>
-                            <input ref={getEmail} onChange={formik.handleChange} value={formik.values.email} id="email" name="email" type="email" className="email-field" style={{ ...{ fontFamily: "Georgia" } }} placeholder="Email" />
+                            <input ref={getEmail} onChange={formik.handleChange} value={formik.values.email} id="email" name="email" type="email" className={formik.errors.email ? "errors email-field" : 'email-field'} style={{ ...{ fontFamily: "Georgia" } }} placeholder="Email" />
                         </Grid>
 
                         {/* password */}
                         <Grid item lg={12} md={12} sm={12} xs={12} className='d-flex justify-content-center'>
-                            <input ref={getPassword} onChange={formik.handleChange} value={formik.values.password} id="password" name="password" type="password" className="password-field" style={{ ...{ fontFamily: "Georgia" } }} placeholder="Password"></input>
+                            <input ref={getPassword} onChange={formik.handleChange} value={formik.values.password} id="password" name="password" type="password" className={formik.errors.password ? "errors password-field" : "password-field"} style={{ ...{ fontFamily: "Georgia" } }} placeholder="Password"></input>
                         </Grid>
 
                         <Grid item lg={12} md={12} sm={12} xs={12} className='d-flex justify-content-center'>
