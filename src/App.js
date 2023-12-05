b4af9d4b7dc2686fcbb02105de939d60591f7593
Hritik <pypi.pioneer@gmail.com>
2023-12-05 15:29:30 +0530

    searchbar added as navbar

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import './App.css';
 import { Routes, Route } from 'react-router-dom';
 import Navbar from './components/header/Navbar';
+import Searchbar from './components/header/Searchbar';
 
 function App() {
   return (
     <>
         
       <Navbar />
-      {/* 
+      <Searchbar />
 
+      {/* 
       <Routes>
         <Route exact path={"/signup"} element={<Signup  />} />
         <Route exact path="/login" element={<Login />} />
