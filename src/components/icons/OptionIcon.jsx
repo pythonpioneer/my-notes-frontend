60a67e92d2a22d991295503ac6065e3d798c203d
Hritik <pypi.pioneer@gmail.com>
2023-12-16 00:09:15 +0530

    ui updated

--- src/components/icons/OptionIcon.jsx
+++ src/components/icons/OptionIcon.jsx
@@ -6,7 +6,7 @@ export default function OptionIcon({ style }) {
         <>
             <div className="dropdown show" style={style}>
                 <i className="fa-solid fa-ellipsis-vertical" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"></i>
-                <div className="dropdown-menu" aria-labelledby="dropdownMenuLink">
+                <div className="dropdown-menu" style={{ marginRight: '1vw' }} aria-labelledby="dropdownMenuLink">
                     <span className="dropdown-item">Delete</span>
                 </div>
             </div>
