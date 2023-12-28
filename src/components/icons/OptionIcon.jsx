0592dc54dedb0f4f62b99bc686da8032392937ff
Hritik <pypi.pioneer@gmail.com>
2023-12-28 17:07:28 +0530

    editnote form's theme mode enabled

--- src/components/icons/OptionIcon.jsx
+++ src/components/icons/OptionIcon.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 
 
-export default function OptionIcon({ style, onClick }) {
+export default function OptionIcon({ style, onClick, theme }) {
     return (
         <>
             <div className="dropdown show" style={style}>
-                <i className="fa-solid fa-ellipsis-vertical" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"></i>
-                <div className="dropdown-menu" style={{ marginRight: '1vw' }} aria-labelledby="dropdownMenuLink">
-                    <span className="dropdown-item" onClick={onClick}>Delete</span>
+                <i className={`fa-solid fa-ellipsis-vertical ${theme === 'dark' ? 'placeholder-color' : '' }`} id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"></i>
+                <div className={`${theme === 'dark' ? 'dark-note' : '' } dropdown-menu`} style={{ marginRight: '1vw' }} aria-labelledby="dropdownMenuLink">
+                    <span className={`${theme === 'dark' ? 'dark-note' : '' } dropdown-item`} onClick={onClick}>Delete</span>
                 </div>
             </div>
         </>
