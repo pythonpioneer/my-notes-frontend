5eff01e203966916656e15ccb6ec96641a716e52
Hritik <pypi.pioneer@gmail.com>
2023-12-29 01:22:40 +0530

    note-item component transmuted

--- src/components/body/NoteItem.jsx
+++ src/components/body/NoteItem.jsx
@@ -39,12 +39,12 @@ export default function NoteItem(props) {
 
                 <Grid className="card-body" onClick={displayEditor}>
 
-                    <Grid item lg={6} className="d-inline-block mr-5" style={{ fontFamily: "Georgia", fontSize: '1em', fontStyle: 'italic', fontWeight: 'bold', marginTop: '-12px' }}>{props.title}</Grid>
+                    <Grid item lg={6} className="mr-5 beautify-notes" style={{ fontFamily: "Georgia", fontSize: '1em', fontStyle: 'italic', fontWeight: 'bold' }}>{props.title}</Grid>
                     <Grid item lg={12} className="">
-                        <p className="card-text" style={{ fontSize: '0.8em', color: '#A9A9A9' }}>{props.desc}</p>
+                        <p className="card-text beautify-notes" style={{ fontSize: '0.8em', color: '#A9A9A9' }}>{props.desc}</p>
 
                         <p className="card-text d-inline-block" style={{ fontSize: '0.8em', color: '#A9A9A9', marginBottom: '-3px' }}>{getCurrentDate(props.datetime)}</p>
-                        {props.tag && <div className="text-center d-inline-block mx-5" style={{ paddingLeft: '10px', paddingRight: '10px', borderStyle: 'solid', borderRadius: '10px', color: 'white', backgroundColor: '#4B0082', marginBottom: '-3px', float: 'right' }}>{props.tag}</div>}
+                        {props.tag && <div className="text-center d-inline-block mx-5 beautify-notes" style={{ paddingLeft: '10px', paddingRight: '10px', borderStyle: 'solid', borderRadius: '10px', color: 'white', backgroundColor: '#4B0082', marginBottom: '-3px', float: 'right' }}>{ props.tag.length > 8 ? props.tag.slice(0, 8) + '...' : props.tag}</div>}
                     </Grid>
                 
                 </Grid>
