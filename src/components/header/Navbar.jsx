ef4777b1e2500492c6e6b25a4f78b1c94f873a88
Hritik <pypi.pioneer@gmail.com>
2023-12-28 11:24:14 +0530

    toggle icon refactored

--- src/components/header/Navbar.jsx
+++ src/components/header/Navbar.jsx
@@ -1,12 +1,19 @@
 import React from 'react';
 import ToggleIcon from '../icons/ToggleIcon';
 
+
 export default function Navbar() {
+
+    //  to handle toggle theme 
+    const handleToggleTheme = () => {
+        console.log('theme')
+    };
+
     return (
         <>
             <nav className="navbar navbar-light">
                 <span className="navbar-brand mt-3" style={{ fontSize: "2em", fontFamily: "Georgia", fontWeight: 'bold', marginLeft: '1%'  }}>Notes</span>
-                <ToggleIcon/>
+                <ToggleIcon onClick={handleToggleTheme} />
             </nav>
         </>
     );
---
+++ src/components/icons/MoonIcon.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+
+// display this icon in dark modes/themes
+export default function MoonIcon() {
+    return (
+        <>
+            <i className="fa-solid fa-cloud-moon"></i>
+        </>
+    )
+}
