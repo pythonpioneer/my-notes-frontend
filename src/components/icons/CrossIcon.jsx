74403e8be00bc18d24f2e013c31888bb9f34315e
Hritik <pypi.pioneer@gmail.com>
2023-12-29 23:48:29 +0530

    now, we can fetch search text from input search bar

---
+++ src/components/icons/CrossIcon.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+
+export default function CrossIcon({ onClick }) {
+    return (
+        <>
+            <i className="fa-solid fa-xmark mr-4" onClick={onClick}></i>
+        </>
+    )
+}
