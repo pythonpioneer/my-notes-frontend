c072e63688f6078d33a0a0928a4a17b5f49ff692
Hritik <pypi.pioneer@gmail.com>
2024-02-01 17:07:54 +0530

    icons audio triggers changed to click-end

--- src/components/icons/CompleteIcon.jsx
+++ src/components/icons/CompleteIcon.jsx
@@ -10,7 +10,7 @@ export default function CompleteIcon({ style, onClick }) {
 				style={style} onClick={() => {
 					onClick();
 				}}
-				onTouchStart={() => {
+				onTouchEnd={() => {
 					playClickAudio(audio);
 				}}
 			></i>
